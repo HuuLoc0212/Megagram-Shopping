44f1ef25b8d2971753969a618259e7fe5e6d05fb
HuuLoc0212 <nguynlc885@gmail.com>
2024-08-26 00:40:10 +0700

    - Populated RecyclerView with data on the phone screen. - Added load more functionality to dynamically load additional items.

--- app/src/main/java/com/example/megagram_app/adapter/DienThoaiAdapter.java
+++ app/src/main/java/com/example/megagram_app/adapter/DienThoaiAdapter.java
@@ -5,6 +5,7 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ImageView;
+import android.widget.ProgressBar;
 import android.widget.TextView;
 
 import androidx.annotation.NonNull;
@@ -16,47 +17,86 @@ import com.example.megagram_app.model.SanPhamMoi;
 
 import java.util.List;
 
-public class DienThoaiAdapter extends RecyclerView.Adapter<DienThoaiAdapter.MyViewHolder> {
+public class DienThoaiAdapter extends RecyclerView.Adapter<RecyclerView.ViewHolder> {
    Context context;
    List<SanPhamMoi> array;
+
+
+
+
+   private static final int VIEW_TYPE_DATA =0;
+   private static final int VIEW_TYPE_LOADING =1;
 
     public DienThoaiAdapter(Context context, List<SanPhamMoi> array) {
         this.context = context;
         this.array = array;
     }
 
+
+
+
+
     @NonNull
     @Override
-    public MyViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
-        View view= LayoutInflater.from(parent.getContext()).inflate(R.layout.item_dienthoai,parent,false)
-        return new MyViewHolder(view);
+    public RecyclerView.ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        if (viewType == VIEW_TYPE_DATA){
+            View view= LayoutInflater.from(parent.getContext()).inflate(R.layout.item_dienthoai,parent,false);
+            return new MyViewHolder(view);
+        }else{
+            View view= LayoutInflater.from(parent.getContext()).inflate(R.layout.item_loading,parent,false);
+            return new LoadingViewHolder(view);
+        }
     }
 
     @Override
-    public void onBindViewHolder(@NonNull MyViewHolder holder, int position) {
-        SanPhamMoi sanPham= array.get(position);
-        holder.tensp.setText(sanPham.getTensp());
-        // Gía
+    public int getItemViewType(int position) {
+        // Neu nhu mang nay get vi tri nao do bang Null thi se xuat ra loai LOADING va nguoc lai la loai DATA
+        return array.get(position) == null ? VIEW_TYPE_LOADING:VIEW_TYPE_DATA;
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull RecyclerView.ViewHolder holder, int position) {
+        if (holder instanceof MyViewHolder){
+            MyViewHolder myViewHolder =(MyViewHolder) holder;
+            SanPhamMoi sanPham= array.get(position);
+            myViewHolder.tensp.setText(sanPham.getTensp());
+            // Gía
 //        DecimalFormat decimalFormat= new DecimalFormat("###,###,###");
-//        holder.giasp.setText("Giá: "+decimalFormat.format(Double.parseDouble(sanPham.getGiasp()))+"Đ");
-        holder.mota.setText(sanPham.getMota());
-        Glide.with(context).load(sanPham.getHinhanh()).into(holder.hinhanh);
+//        myViewHolder.giasp.setText("Giá: "+decimalFormat.format(Double.parseDouble(sanPham.getGiasp()))+"Đ");
+            myViewHolder.idsp.setText(sanPham.getId()+" ");
+            myViewHolder.mota.setText(sanPham.getMota());
+            Glide.with(context).load(sanPham.getHinhanh()).into(myViewHolder.hinhanh);
+        }else {
+            LoadingViewHolder loadingViewHolder = (LoadingViewHolder) holder;
+            loadingViewHolder.progressBar.setIndeterminate(true);
+        }
+
     }
 
     @Override
     public int getItemCount() {
         return array.size();
     }
+    // Neu data null thi doan code nay
+    public class LoadingViewHolder extends RecyclerView.ViewHolder{
+        ProgressBar progressBar;
 
+        public LoadingViewHolder(@NonNull View itemView) {
+            super(itemView);
+            progressBar=itemView.findViewById(R.id.progressbar);
+        }
+    }
+    // neu du lieu co data thi add nay len de hien thi data
     public class MyViewHolder extends RecyclerView.ViewHolder {
-        TextView tensp, giasp, mota;
+        TextView tensp, giasp, mota,idsp;
         ImageView hinhanh;
             public MyViewHolder(@NonNull View itemView) {
             super(itemView);
             tensp=itemView.findViewById(R.id.itemdt_ten);
             giasp=itemView.findViewById(R.id.itemdt_gia);
             mota=itemView.findViewById(R.id.itemdt_mota);
-            hinhanh=itemView.findViewById(R.id.itemsp_image);
+            hinhanh=itemView.findViewById(R.id.itemdt_image);
+            idsp=itemView.findViewById(R.id.itemdt_idsp);
         }
     }
 }
